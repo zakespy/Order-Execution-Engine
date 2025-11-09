c9795cd1bef499c2b06274aa61e01ea4e0edbd68
zakespy <satyam.ms@somaiya.edu>
2025-11-09 20:30:05 +0530

    trying to fix host address error

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import Fastify from "fastify";
 import websocket from "@fastify/websocket"; 
-import { orderWorker } from "./queue/worker.js";
+// import { orderWorker } from "./queue/worker.js";
 import orderRoutes from "./api/orders.controller.js";
-import { orderQueue } from "./queue/order.queue.js";
+// import { orderQueue } from "./queue/order.queue.js";
 
 const fastify = Fastify();
 fastify.register(websocket);
 fastify.register(orderRoutes);
 
 // const port = Number(process.env.PORT) || 3000;
-const port = Number(process.env.PORT)
-const host = "0.0.0.0"; // Required for Cloud Run networking
+const port = Number(process.env.PORT) || 8080
+const host = process.env.HOST || "0.0.0.0"; // Required for Cloud Run networking
 
 fastify.listen({ port, host }, (err, address) => {
   if (err) {
